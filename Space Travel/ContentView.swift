5ec5552b25c5c2fd643904cadfe100a4c5780006
Erdinç Yılmaz <erdinc.yilmaz@digiturk.com.tr>
2026-02-15 02:08:28 +0300

    Initial Commit

---
+++ Space Travel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Space Travel
+//
+//  Created by Erdinç Yılmaz on 15.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Space Travel/Space_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_TravelApp.swift
+//  Space Travel
+//
+//  Created by Erdinç Yılmaz on 15.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Space_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
